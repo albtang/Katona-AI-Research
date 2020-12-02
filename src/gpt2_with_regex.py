1c41ae6ba01019d399a58943a65924e573dfa42c
Albert Tang <alberttang@berkeley.edu>
2020-12-02 03:43:20 +0000

    dec1 30*15

--- src/gpt2_with_regex.py
+++ src/gpt2_with_regex.py
@@ -195,6 +195,6 @@ aliases = [['Jeep', 'Fiat', 'Chrysler'],
 #        ['Infiniti'],
         ['Volvo']]
 
-process_responses(generate_responses("The car brand "," is similar to",[i[0] for i in aliases],30,"345M"), aliases, 'Nov30 30 14')
+process_responses(generate_responses("The car brand "," is similar to",[i[0] for i in aliases],30,"345M"), aliases, 'Dec1 30 15')
 
 
