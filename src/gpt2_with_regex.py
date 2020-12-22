92461f0fb3f492d78c341d20743b81086bb71593
Albert Tang <alberttang@berkeley.edu>
2020-12-22 00:30:28 +0000

    45*15, two baseline runs for temperature = 0.6 and 0.7

--- src/gpt2_with_regex.py
+++ src/gpt2_with_regex.py
@@ -144,7 +144,7 @@ def generate_responses(bgn_prompt,end_prompt, brand_list, nsample,model_size):
     nsample,
     1,
     300,
-    1.5,
+    0.7,
     0,
     '../models',
     prompt_list
@@ -195,6 +195,6 @@ aliases = [['Jeep', 'Fiat', 'Chrysler'],
 #        ['Infiniti'],
         ['Volvo']]
 
-process_responses(generate_responses("The car brand"," is similar to",[],100,"345M"), aliases, 'b Dec17 100 t1.5 1')
+process_responses(generate_responses("The car brand"," is similar to",[""],100,"345M"), aliases, 'b Dec21 t07 100')
 
 
